sjg20/u-boot-concept

Add the cpu init and uart init functions to bring up Tegra2.
       modified:   board/tegra2/harmony/config.mk    // changed the u-boot entry point from 0x00e08000 to 0x00108000
       modified:   cpu/arm_cortexa9/start.S          // call tegra2_init
       modified:   cpu/arm_cortexa9/tegra2/Makefile  // add ap20.c file
       modified:   cpu/arm_cortexa9/tegra2/cpu.h     // added more constant definitions
       modified:   cpu/arm_cortexa9/tegra2/interrupts.c // always disable interrupt
       cpu/arm_cortexa9/tegra2/ap20.c                // init cpu and start
       cpu/arm_cortexa9/tegra2/ap20.h
       modified:   include/asm-arm/arch-tegra2/nvcommon.h
       modified:   include/asm-arm/arch-tegra2/tegra2.h   // more low level supports for bringing up cpu.
       include/asm-arm/arch-tegra2/nvbl_arm_cp15.h
       include/asm-arm/arch-tegra2/nvbl_arm_cpsr.h
       include/asm-arm/arch-tegra2/nvbl_memmap_nvap.h
       include/asm-arm/arch-tegra2/nvboot_bit.h
       include/asm-arm/arch-tegra2/nvboot_clocks.h
       include/asm-arm/arch-tegra2/nvboot_config.h
       include/asm-arm/arch-tegra2/nvboot_osc.h
       modified:   include/serial.h                 // enable tegra2 serial console
       modified:   common/serial.c                  // enable serial port

Review URL: http://codereview.chromium.org/3185012
Patch from Jim Zhang <jimmyz.nvidia@gmail.com>.

Change-Id: I32ae57973661754185d758982d8047e6d9c4c1ac

common/serial.c
@@ -31,7 +31,11 @@ static struct serial_device *serial_devices = NULL;
 static struct serial_device *serial_current = NULL;
 
 #if !defined(CONFIG_LWMON) && !defined(CONFIG_PXA27X)
+#if defined(CONFIG_TEGRA2) && defined(TEGRA2_AVP_ONLY)
+struct serial_device *default_serial_console (void)
+#else
 struct serial_device *__default_serial_console (void)
+#endif
 {
 #if defined(CONFIG_8xx_CONS_SMC1) || defined(CONFIG_8xx_CONS_SMC2)
 	return &serial_smc_device;
@@ -41,7 +45,8 @@ struct serial_device *__default_serial_console (void)
 #elif defined(CONFIG_405GP) || defined(CONFIG_405CR) || defined(CONFIG_440) \
    || defined(CONFIG_405EP) || defined(CONFIG_405EZ) || defined(CONFIG_405EX) \
    || defined(CONFIG_MPC5xxx) || defined(CONFIG_MPC83xx) \
-   || defined(CONFIG_MPC85xx) || defined(CONFIG_MPC86xx)
+   || defined(CONFIG_MPC85xx) || defined(CONFIG_MPC86xx) \
+   || defined(CONFIG_TEGRA2)
 #if defined(CONFIG_CONS_INDEX) && defined(CONFIG_SYS_NS16550_SERIAL)
 #if (CONFIG_CONS_INDEX==1)
 	return &eserial1_device;
@@ -88,7 +93,9 @@ struct serial_device *__default_serial_console (void)
 #endif
 }
 
+#ifndef TEGRA2_AVP_ONLY
 struct serial_device *default_serial_console(void) __attribute__((weak, alias("__default_serial_console")));
+#endif 
 #endif
 
 int serial_register (struct serial_device *dev)

include/serial.h
@@ -25,7 +25,8 @@ extern struct serial_device * default_serial_console (void);
 #if defined(CONFIG_405GP) || defined(CONFIG_405CR) || defined(CONFIG_440) || \
     defined(CONFIG_405EP) || defined(CONFIG_405EZ) || defined(CONFIG_405EX) || \
     defined(CONFIG_MPC5xxx) || defined(CONFIG_MPC83xx) || \
-    defined(CONFIG_MPC85xx) || defined(CONFIG_MPC86xx)
+    defined(CONFIG_MPC85xx) || defined(CONFIG_MPC86xx) || \
+    defined(CONFIG_TEGRA2)
 extern struct serial_device serial0_device;
 extern struct serial_device serial1_device;
 #if defined(CONFIG_SYS_NS16550_SERIAL)

chpec/uboot-ac100

common/serial.c
@@ -31,7 +31,11 @@ static struct serial_device *serial_devices = NULL;
 static struct serial_device *serial_current = NULL;
 
 #if !defined(CONFIG_LWMON) && !defined(CONFIG_PXA27X)
+#if defined(CONFIG_TEGRA2) && defined(TEGRA2_AVP_ONLY)
+struct serial_device *default_serial_console (void)
+#else
 struct serial_device *__default_serial_console (void)
+#endif
 {
 #if defined(CONFIG_8xx_CONS_SMC1) || defined(CONFIG_8xx_CONS_SMC2)
 	return &serial_smc_device;
@@ -42,7 +46,8 @@ struct serial_device *__default_serial_console (void)
    || defined(CONFIG_405EP) || defined(CONFIG_405EZ) || defined(CONFIG_405EX) \
    || defined(CONFIG_MB86R0x) || defined(CONFIG_MPC5xxx) \
    || defined(CONFIG_MPC83xx) || defined(CONFIG_MPC85xx) \
-   || defined(CONFIG_MPC86xx) || defined(CONFIG_SYS_SC520)
+   || defined(CONFIG_MPC86xx) || defined(CONFIG_SYS_SC520) \
+   || defined(CONFIG_TEGRA2)
 #if defined(CONFIG_CONS_INDEX) && defined(CONFIG_SYS_NS16550_SERIAL)
 #if (CONFIG_CONS_INDEX==1)
 	return &eserial1_device;
@@ -97,7 +102,9 @@ struct serial_device *__default_serial_console (void)
 #endif
 }
 
+#ifndef TEGRA2_AVP_ONLY
 struct serial_device *default_serial_console(void) __attribute__((weak, alias("__default_serial_console")));
+#endif 
 #endif
 
 int serial_register (struct serial_device *dev)

include/serial.h
@@ -27,7 +27,8 @@ extern struct serial_device * default_serial_console (void);
     defined(CONFIG_405EP) || defined(CONFIG_405EZ) || defined(CONFIG_405EX) || \
     defined(CONFIG_MB86R0x) || defined(CONFIG_MPC5xxx) || \
     defined(CONFIG_MPC83xx) || defined(CONFIG_MPC85xx) || \
-    defined(CONFIG_MPC86xx) || defined(CONFIG_SYS_SC520)
+    defined(CONFIG_MPC86xx) || defined(CONFIG_SYS_SC520) || \
+    defined(CONFIG_TEGRA2)
 extern struct serial_device serial0_device;
 extern struct serial_device serial1_device;
 #if defined(CONFIG_SYS_NS16550_SERIAL)
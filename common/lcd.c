c1413bf674980ebe890e121f8a21543e02002adc
Tom Wai-Hong Tam <waihong@google.com>
2011-01-06 17:52:52 +0800

    Fix BMP rendering bug, wrong fb offset calculation for bpix == 16.
    For bpix == 16, each pixel is 2-byte. fb offset should shift more x bytes.
    
    BUG=chromium-os:10499
    TEST=build, boot, and "bmp display BMP_ADDR 100 100" to see the right offset.
    
    Change-Id: I72fe585d0d8d6f44c66b1a379e575c8f00f1a91d
    
    Review URL: http://codereview.chromium.org/6110001

--- common/lcd.c
+++ common/lcd.c
@@ -800,8 +800,10 @@ int lcd_display_bitmap(ulong bmp_image, int x, int y)
 		height = panel_info.vl_row - y;
 
 	bmap = (uchar *)bmp + le32_to_cpu(bmp->header.data_offset);
-	fb   = (uchar *) (lcd_base +
-		(y + height - 1) * lcd_line_length + x);
+	fb   = (uchar *) (lcd_base + (y + height - 1) * lcd_line_length + x);
+	/* additional fb shift for bpix == 16 since each pixel is 2-byte */
+	if (bpix == 16)
+		fb += x;
 
 	switch (bmp_bpix) {
 	case 1: /* pass through */

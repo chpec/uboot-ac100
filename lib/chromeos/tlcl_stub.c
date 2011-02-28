a6e15bd148614f43b429b17d9de907a52e9020a8
Rong Chang <rongchang@chromium.org>
2011-03-01 06:00:40 +0800

    Add u-boot tpm driver, and integrate with verify boot. Currently only support developer version infineon i2c tpm ic, with i2c address 0x1A. New versions would use i2c address 0x20 and might have different protocol.
    This CL depends on http://codereview.chromium.org/6469089 .
    
    Remove white spaces
    
    Change-Id: I32e2d34dcda15433d3b0b5514b70cba5b117a044
    
    Clean debug output
    
    Change-Id: Iaf766f599d28e56e47c899cf0890f3f858e67a24
    
    Move vboot command support into developer build, and controled by compile time switch VBOOT_DEBUG
    
    Change-Id: I35d2c7acfbd697adc39f7aa5778792d356b379f0
    
    Change debug macro name to follow verify boot's VBOOT_DEBUG
    
    Change-Id: I8637f436683ac9ed73900ff9952c9af71a326779
    
    Add infineon i2c tpm driver drivers/misc/infineon_i2ctpm.c, tpm tests for u-boot and tlcl stub impl
    
    Change-Id: Ic0d5cf8782815416e1a211fc5aa72170ce7a3ab1
    
    BUG=chromium-os:10497
    TEST=see below
    
    1. build> VBOOT_DEBUG=1 emerge-tegra2_seaboard chromeos-u-boot-next
    2. nvflash u-boot-developer.bin to seaboard, not u-boot.bin
    3. copy signed image.bin to usb stick (fat format)
    4. reboot newly flashed seaboard, wait for the prompt and type
      4a. usb start
      4b. fatload usb 0:1 0x10000000 image.bin
      4c. cros load_fw 0x10000000 0x00400000 0x10500000
    
    Check the tpm protocol dump and verify boot return value.
    
    Review URL: http://codereview.chromium.org/6542065

--- lib/chromeos/tlcl_stub.c
+++ lib/chromeos/tlcl_stub.c
@@ -36,26 +36,35 @@
 #include <config.h>
 #include <common.h>
 #include <tlcl_stub.h>
+#include <tpm.h>
 
-/* A dummy TPM library implementation for now */
+/* A simple TPM library implementation for now */
 
 uint32_t TlclStubInit(void)
 {
-	return 0;
+	return tpm_init();
 }
 
 uint32_t TlclCloseDevice(void)
 {
-	return 0;
+	return TPM_SUCCESS;
 }
 
 uint32_t TlclOpenDevice(void)
 {
-	return 0;
+	return TPM_SUCCESS;
 }
 
 uint32_t TlclStubSendReceive(const uint8_t* request, int request_length,
 		uint8_t* response, int max_length)
 {
-	return TPM_E_NON_FATAL;
+	int ret_code;
+	ret_code = tpm_send(request, request_length);
+	if (ret_code)
+		return TPM_E_IOERROR;
+	ret_code = tpm_receive(response, max_length);
+	if (ret_code)
+		return TPM_E_IOERROR;
+	return TPM_SUCCESS;
 }
+

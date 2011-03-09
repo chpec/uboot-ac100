c53ce135ad7efa9b73fb611a18e5006b458c59f9
Che-Liang Chiou <clchiou@chromium.org>
2011-03-09 11:18:15 +0800

    Rename caller_internal_t to firmware_storage_t
    Renaming the caller_internal_t for better reflecting its purpose.
    
    BUG=chromium-os:1302
    TEST=MAKEALL successfully
    
    Review URL: http://codereview.chromium.org/6594066
    
    Change-Id: I0e28c5934fdbd198ab4fdb278a33d4707767b3c6

--- board/tegra2/generic/chromeos/eeprom_driver.c
+++ board/tegra2/generic/chromeos/eeprom_driver.c
@@ -78,7 +78,7 @@ static ssize_t mem_read(void *context, void *buf, size_t count)
 	return count;
 }
 
-int init_caller_internal(caller_internal_t *ci)
+int init_firmware_storage(firmware_storage_t *f)
 {
 	struct context_t *context;
 
@@ -87,15 +87,15 @@ int init_caller_internal(caller_internal_t *ci)
 	context->cur = (void *) TEXT_BASE;
 	context->end = (void *) TEXT_BASE + CONFIG_FIRMWARE_SIZE;
 
-	ci->seek = mem_seek;
-	ci->read = mem_read;
-	ci->context = (void *) context;
+	f->seek = mem_seek;
+	f->read = mem_read;
+	f->context = (void *) context;
 
 	return 0;
 }
 
-int release_caller_internal(caller_internal_t *ci)
+int release_firmware_storage(firmware_storage_t *f)
 {
-	free(ci->context);
+	free(f->context);
 	return 0;
 }
